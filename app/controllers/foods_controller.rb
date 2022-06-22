b307672db4cc2cc487ddb295f443d7df06ec7e1c
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-22 09:05:05 -0400

    change and update  root views

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,27 @@
+class FoodsController < ApplicationController
+    before_action :authenticate_user!, except: :index
+  
+    def index
+      @foods = Food.all
+    end
+  
+    def new
+      @food = Food.new
+    end
+  
+    def create
+      new_food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price))
+      new_food.user = current_user
+  
+      new_food.save!
+      redirect_to foods_path
+    end
+  
+    def destroy
+      food = Food.find(params[:id])
+      return unless food.present? && food.user == current_user
+  
+      food.destroy!
+      redirect_to foods_path
+    end
+end
