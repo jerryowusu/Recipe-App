f55820409af6b05b932b62320b7e06839ea1df3f
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-22 08:22:33 -0400

    Update user and food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,10 @@
 class Food < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
+
+  has_many :inventory_foods
+  has_many :inventories, through: :inventory_foods
 
   validates :name, presence: true, length: { in: 1..255 }
   validates :measurement_unit, presence: true, length: { maximum: 255 }
