903f00639c7d079c7bf3704dc0b2b668298fe4a4
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-22 09:51:39 -0400

    Update navbar view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "foods#index"
   resources :foods, only: [:index, :new, :create, :destroy]
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
+  root to: "foods#index"
 end
