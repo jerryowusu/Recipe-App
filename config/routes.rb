539512cb4e987971ad1196174cb38a88dfa240f2
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-23 02:59:06 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
-  # get 'recipe_foods/new'
-  # get 'recipe_foods/create'
-  # get 'recipe_foods/edit'
-  # get '/public_recipes', to: 'recipes#public_recipes'
-  # get 'foods/index'
-  # get 'foods/new'
-  # get 'foods/create'
-  # get 'foods/destroy'
+ 
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :foods, only: %i[index new create destroy]
-  resources :recipes, only: %i[index show new create destroy] do
-    resources :recipe_foods, only: %i[new create edit update destroy]
-  end
+  get 'shopping/list'
+  get "/general_shopping_list", to: 'shopping#list'
+  resources :publics, only: %i[index]
+  resources :recipes, only: %i[index new create destroy show]
+  resources :foods, only: %i[index new create destroy show]
+  resources :recipe_foods, only: %i[index new create destroy show edit]
 
-  get '/public_recipes', to: 'recipes#public_recipes'
+  get '/public_recipes', to: 'publics#index'
   root to: 'foods#index'
 end
