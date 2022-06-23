0667995670f96269105eb15d412665aae1e3cd8d
jerryowusu <jerryowusu6133@gmail.com>
2022-06-23 11:48:32 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
   validates :name, presence: true
-  
+
   def admin?
     role == 'admin'
   end
