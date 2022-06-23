88e9ffec1fcaab5b134547a85c7e4f2519a5416c
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-23 17:31:44 -0400

    Update root view to public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  get 'shopping/list'
+  # get 'shopping/list'
   get "/general_shopping_list", to: 'shoppings#list'
   resources :publics, only: %i[index]
   resources :recipes, only: %i[index new create destroy show]
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :recipe_foods, only: %i[index new create destroy show edit]
 
   get '/public_recipes', to: 'publics#index'
-  root to: 'foods#index'
+  root to: 'publics#index'
 end
